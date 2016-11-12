24384b37dc326f8e1a9c104f8d0a240988cb8c43
Silverfeelin <Silverfeelin@gmail.com>
2016-11-12 02:24:49 +0100

    Encrypt config, item swapping change
    Encrypt configuration, written to `starbound.config` on first run,
    allows users to set what parameters they want to encrypt for their
    items. Default values only encrypt item name and directives.
    Performance gain can be made by disabling encryption (by setting all
    values to false).
    For backwards compatibility, nil values are treated as true (since the
    encrypt table is only present on new stored outfits).
    
    The way items are taken and given to the user has been changed. Outfit
    items are stackable, but the game will not give you any if you already
    have an identical outfit item. Likewise, the mod will not take an outfit
    item if it is your last one.

--- interface/storedOutfits/loadOutfit.lua
+++ interface/storedOutfits/loadOutfit.lua
@@ -2,26 +2,65 @@ require "/scripts/util.lua"
 require "/scripts/vec2.lua"
 require "/scripts/encryption.lua"
 
+local default_config = {
+  encrypt = {
+    name = true,
+    description = false,
+    directives = true,
+    shortdescription = false
+  }
+}
+
+local config
+
 local encrypt = encryption.encrypt
+local encryptItem = function(v)
+  if config.encrypt.name then v.name = encrypt(v.name) end
+  if v.parameters then
+    if config.encrypt.shortdescription and v.parameters.shortdescription then v.parameters.shortdescription = encrypt(v.parameters.shortdescription) end
+    if config.encrypt.description and v.parameters.description then v.parameters.description = encrypt(v.parameters.description) end
+    if config.encrypt.directives and v.parameters.directives then v.parameters.directives = encrypt(v.parameters.directives) end
+  end
+  v.encrypted = config.encrypt
+  return v
+end
+
 local decrypt = encryption.decrypt
+local decryptItem = function(v)
+  encrypted = v.encrypted or {}
+  if encrypted.name ~= false then v.name = decrypt(v.name) end
+  if v.parameters then
+    if encrypted.shortdescription ~= false and v.parameters.shortdescription then v.parameters.shortdescription = decrypt(v.parameters.shortdescription) end
+    if encrypted.description ~= false and v.parameters.description then v.parameters.description = decrypt(v.parameters.description) end
+    if encrypted.directives ~= false and v.parameters.directives then v.parameters.directives = decrypt(v.parameters.directives) end
+  end
+  return v
+end
 
 function init()
   -- Ensure a key exists.
   if not encryption.getKey() then encryption.generateKey() end
 
+  -- Set configuration
+  local cfg = root.getConfigurationPath("storedOutfits")
+  if not cfg then
+    root.setConfigurationPath("storedOutfits", default_config)
+    config = default_config
+  else
+    config = cfg
+  end
+
   local heldItem = player.primaryHandItem()
 
   -- Remove previous outfit item.
-  player.consumeItem(heldItem)
+  if player.hasCountOfItem(heldItem) > 1 then
+    heldItem.count = 1
+    player.consumeItem(heldItem)
+  end
 
   local newOutfit = heldItem and heldItem.parameters and heldItem.parameters.outfit or {}
   for k,v in pairs(newOutfit) do
-    v.name = decrypt(v.name)
-    if v.parameters then
-      if v.parameters.shortdescription then v.parameters.shortdescription = decrypt(v.parameters.shortdescription) end
-      if v.parameters.description then v.parameters.description = decrypt(v.parameters.description) end
-      if v.parameters.directives then v.parameters.directives = decrypt(v.parameters.directives) end
-    end
+    v = decryptItem(v)
     newOutfit[k] = v
   end
 
@@ -74,12 +113,7 @@ function init()
   if #item.parameters.inventoryIcon == 0 then item.parameters.inventoryIcon = oldIcon end
 
   for k,v in pairs(oldOutfit) do
-    v.name = encrypt(v.name)
-    if v.parameters then
-      if v.parameters.shortdescription then v.parameters.shortdescription = encrypt(v.parameters.shortdescription) end
-      if v.parameters.description then v.parameters.description = encrypt(v.parameters.description) end
-      if v.parameters.directives then v.parameters.directives = encrypt(v.parameters.directives) end
-    end
+    v = encryptItem(v)
 
     oldOutfit[k] = v
   end
@@ -87,7 +121,11 @@ function init()
   item.parameters.outfit = oldOutfit
 
   -- Give new outfit item.
-  player.giveItem(item)
+  item.count = nil
+  if not player.hasItem(item) then
+    item.count = 1
+    player.giveItem(item)
+  end
 
   -- Close invisible interface.
   pane.dismiss()
