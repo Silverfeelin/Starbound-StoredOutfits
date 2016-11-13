87c0a9c0f761f8394263adba8ecbabab1836fa3f
Silverfeelin <Silverfeelin@gmail.com>
2016-11-13 02:35:54 +0100

    Preview improved with only directives encryption

--- interface/storedOutfits/loadOutfit.lua
+++ interface/storedOutfits/loadOutfit.lua
@@ -105,6 +105,8 @@ end
 local createIcon = function(outfit)
   local head, chest, legs, back = outfit.head, outfit.chest, outfit.legs, outfit.back
 
+  local encDir = config.encrypt.directives
+
   local icon = jarray()
   table.insert(icon, { position = {0, 0}, image = "/assetMissing.png?replace;00000000=ffffff;ffffff00=ffffff?setcolor=ffffff?scalenearest=1?crop=0;0;13;17?blendmult=/objects/outpost/customsign/signplaceholder.png;0;0?replace;01000101=000000FF;01000201=000000FF;01000301=000000FF;01000401=000000FF;01000501=000000FF;01000601=000000FF;01000701=000000FF;01000801=000000FF;02000101=000000FF;02000201=251C0BFF;02000301=403316FF;02000401=403316FF;02000501=463818FF;02000601=463818FF;02000701=463818FF;02000801=403316FF;03000101=000000FF;03000201=251C0BFF;03000301=463818FF;03000401=403316FF;03000501=403316FF;03000601=403316FF;03000701=403316FF;03000801=403316FF;04000101=000000FF;04000201=251C0BFF;04000301=463818FF;04000401=463818FF;04000501=463818FF;04000601=403316FF;04000701=463818FF;04000801=463818FF;05000101=000000FF;05000201=251C0BFF;05000301=251C0BFF;05000401=251C0BFF;05000501=251C0BFF;05000601=251C0BFF;05000701=251C0BFF;05000801=251C0BFF;06000101=000000FF;06000201=33120FFF;06000301=33120FFF;06000401=33120FFF;06000501=33120FFF;06000601=33120FFF;06000701=33120FFF;06000801=33120FFF;07000101=000000FF;07000201=521D19FF;07000301=521D19FF;07000401=521D19FF;07000501=521D19FF;07000601=521D19FF;07000701=521D19FF;07000801=521D19FF;08000101=000000FF;08000201=33120FFF;08000301=33120FFF;08000401=33120FFF;08000501=33120FFF;08000601=33120FFF;08000701=33120FFF;08000801=33120FFF;09000101=000000FF;09000201=521D19FF;09000301=521D19FF;09000401=521D19FF;09000501=521D19FF;09000601=521D19FF;09000701=521D19FF;09000801=521D19FF;10000101=000000FF;10000201=33120FFF;10000301=33120FFF;10000401=33120FFF;10000501=33120FFF;10000601=33120FFF;10000701=33120FFF;10000801=33120FFF;11000101=000000FF;11000201=251C0BFF;11000301=251C0BFF;11000401=251C0BFF;11000501=251C0BFF;11000601=251C0BFF;11000701=251C0BFF;11000801=251C0BFF;12000101=000000FF;12000201=6D6330FF;12000301=6D6330FF;12000401=6D6330FF;12000501=6D6330FF;12000601=6D6330FF;12000701=6D6330FF;12000801=6D6330FF;13000101=000000FF;13000201=000000FF;13000301=000000FF;13000401=000000FF;13000501=000000FF;13000601=000000FF;13000701=000000FF;13000801=000000FF?blendmult=/objects/outpost/customsign/signplaceholder.png;0;-8?replace;01000101=000000FF;01000201=000000FF;01000301=000000FF;01000401=000000FF;01000501=000000FF;01000601=000000FF;01000701=000000FF;01000801=000000FF;02000101=403316FF;02000201=463818FF;02000301=463818FF;02000401=403316FF;02000501=403316FF;02000601=6D6330FF;02000701=6D6330FF;02000801=897C4CFF;03000101=403316FF;03000201=403316FF;03000301=403316FF;03000401=463818FF;03000501=463818FF;03000601=6D6330FF;03000701=6D6330FF;03000801=897C4CFF;04000101=463818FF;04000201=463818FF;04000301=403316FF;04000401=463818FF;04000501=463818FF;04000601=6D6330FF;04000701=897C4CFF;04000801=897C4CFF;05000101=251C0BFF;05000201=251C0BFF;05000301=251C0BFF;05000401=251C0BFF;05000501=251C0BFF;05000601=6D6330FF;05000701=897C4CFF;05000801=897C4CFF;06000101=33120FFF;06000201=33120FFF;06000301=554E4EFF;06000401=61686CFF;06000501=251C0BFF;06000601=6D6330FF;06000701=897C4CFF;06000801=897C4CFF;07000101=521D19FF;07000201=521D19FF;07000301=554E4EFF;07000401=61686CFF;07000501=251C0BFF;07000601=6D6330FF;07000701=897C4CFF;07000801=897C4CFF;08000101=33120FFF;08000201=33120FFF;08000301=554E4EFF;08000401=61686CFF;08000501=251C0BFF;08000601=6D6330FF;08000701=897C4CFF;08000801=897C4CFF;09000101=521D19FF;09000201=521D19FF;09000301=554E4EFF;09000401=61686CFF;09000501=251C0BFF;09000601=6D6330FF;09000701=897C4CFF;09000801=897C4CFF;10000101=33120FFF;10000201=33120FFF;10000301=554E4EFF;10000401=61686CFF;10000501=251C0BFF;10000601=6D6330FF;10000701=6D6330FF;10000801=897C4CFF;11000101=251C0BFF;11000201=251C0BFF;11000301=251C0BFF;11000401=251C0BFF;11000501=251C0BFF;11000601=6D6330FF;11000701=6D6330FF;11000801=897C4CFF;12000101=6D6330FF;12000201=6D6330FF;12000301=6D6330FF;12000401=6D6330FF;12000501=6D6330FF;12000601=6D6330FF;12000701=6D6330FF;12000801=6D6330FF;13000101=000000FF;13000201=000000FF;13000301=000000FF;13000401=000000FF;13000501=000000FF;13000601=000000FF;13000701=000000FF;13000801=000000FF?blendmult=/objects/outpost/customsign/signplaceholder.png;0;-16?replace;01000101=000000FF;02000101=000000FF;03000101=000000FF;04000101=000000FF;05000101=000000FF;06000101=000000FF;07000101=000000FF;08000101=000000FF;09000101=000000FF;10000101=000000FF;11000101=000000FF;12000101=000000FF;13000101=000000FF" })
 
@@ -113,20 +115,21 @@ local createIcon = function(outfit)
     table.insert(icon, { position = {16, -1}, image = "/objects/generic/mannequin/mannequin.png" })
   end
 
-  -- Return early if encryption is enabled.
-  if config.encrypt.name or config.encrypt.directives then return icon end
+  -- Return early if name encryption is enabled.
+  if config.encrypt.name then return icon end
 
   local getFrames = function(cfg)
     return cfg.config.maleFrames or cfg.config.femaleFrames or cfg.config.frames
   end
 
+  local encDir = config.encrypt.directives
   local itemFrames = {}
 
   if head then
     local cfg = root.itemConfig(head.name)
     local frames = getFrames(cfg)
     if frames then
-      itemFrames[5] = { image = cfg.directory .. frames .. ":normal" .. (head.parameters and head.parameters.directives or ""), position = {16, 0} }
+      itemFrames[5] = { image = cfg.directory .. frames .. ":normal" .. (not encDir and head.parameters and head.parameters.directives or ""), position = {16, 0} }
     end
   end
 
@@ -135,13 +138,13 @@ local createIcon = function(outfit)
     local frames = getFrames(cfg)
     if frames then
       if frames.backSleeve then
-        itemFrames[1] = { image = cfg.directory .. frames.backSleeve .. ":idle.1" .. (chest.parameters and chest.parameters.directives or ""), position = {16, 0} }
+        itemFrames[1] = { image = cfg.directory .. frames.backSleeve .. ":idle.1" .. (not encDir and chest.parameters and chest.parameters.directives or ""), position = {16, 0} }
       end
       if frames.body then
-        itemFrames[4] = { image = cfg.directory .. frames.body .. ":idle.1" .. (chest.parameters and chest.parameters.directives or ""), position = {16, 0} }
+        itemFrames[4] = { image = cfg.directory .. frames.body .. ":idle.1" .. (not encDir and chest.parameters and chest.parameters.directives or ""), position = {16, 0} }
       end
       if frames.frontSleeve then
-        itemFrames[6] = { image = cfg.directory .. frames.frontSleeve .. ":idle.1" .. (chest.parameters and chest.parameters.directives or ""), position = {16, 0} }
+        itemFrames[6] = { image = cfg.directory .. frames.frontSleeve .. ":idle.1" .. (not encDir and chest.parameters and chest.parameters.directives or ""), position = {16, 0} }
       end
     end
   end
@@ -150,7 +153,7 @@ local createIcon = function(outfit)
     local cfg = root.itemConfig(legs.name)
     local frames = getFrames(cfg)
     if frames then
-      itemFrames[3] = { image = cfg.directory .. frames .. ":idle.1" .. (legs.parameters and legs.parameters.directives or ""), position = {16, 0} }
+      itemFrames[3] = { image = cfg.directory .. frames .. ":idle.1" .. (not encDir and legs.parameters and legs.parameters.directives or ""), position = {16, 0} }
     end
   end
 
@@ -158,7 +161,7 @@ local createIcon = function(outfit)
     local cfg = root.itemConfig(back.name)
     local frames = getFrames(cfg)
     if frames then
-      itemFrames[2] = { image = cfg.directory .. frames .. ":idle.1" .. (back.parameters and back.parameters.directives or ""), position = {16, 0} }
+      itemFrames[2] = { image = cfg.directory .. frames .. ":idle.1" .. (not encDir and back.parameters and back.parameters.directives or ""), position = {16, 0} }
     end
   end
 
